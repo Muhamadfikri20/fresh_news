ecc81858bdef28c11771fe3484f38d130cc56bdb
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-03 14:37:16 +0700

    Lecture_18_AddingSearchTextBox_And_TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fresh_news/pages/newsList.dart';
+import 'package:fresh_news/pages/NewsListPage.dart';
 
 import 'package:fresh_news/viewmodels/newsArticleListViewModel.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +9,6 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Fresh News", home: ChangeNotifierProvider(create: (_) => NewsArticleListViewModel(), child: NewsList()));
+    return MaterialApp(title: "Fresh News", home: ChangeNotifierProvider(create: (_) => NewsArticleListViewModel(), child: NewsListPage()));
   }
 }
---
+++ lib/pages/NewsListPage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_news/viewmodels/newsArticleListViewModel.dart';
+import 'package:fresh_news/widgets/newsList.dart';
+import 'package:provider/provider.dart';
+
+class NewsListPage extends StatefulWidget {
+  @override
+  State<NewsListPage> createState() => _NewsListPageState();
+}
+
+class _NewsListPageState extends State<NewsListPage> {
+  final _controller = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+
+    Provider.of<NewsArticleListViewModel>(context, listen: false).populateTopHeadlines();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = Provider.of<NewsArticleListViewModel>(context);
+
+    return Scaffold(
+        appBar: AppBar(title: Text("Top News")),
+        body: Column(children: <Widget>[
+          TextField(
+            controller: _controller,
+            onSubmitted: (value) {
+              // fetch all the news related to the keyword
+            },
+            decoration: InputDecoration(
+                labelText: "Enter search term",
+                icon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.search),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    _controller.clear();
+                  },
+                )),
+          ),
+          Expanded(child: NewsList(articles: vm.articles))
+        ]));
+  }
+}
